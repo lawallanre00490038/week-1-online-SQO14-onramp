631fb631de54a16f437e2a48a8799020fdc0232e
Lawal Olanrewaju Israel <61704062+lawallanre00490038@users.noreply.github.com>
2022-10-29 15:03:10 +0100

    Update challenge-1.js
    first assignment

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,10 +1,11 @@
 function countTruthy(arr) {
     let count = 0;
+    const b = [];
     for(let a of arr){
       if(a){
       count ++;
       }
-    return count;
   }
+  return(count)
 }
 module.exports = countTruthy
