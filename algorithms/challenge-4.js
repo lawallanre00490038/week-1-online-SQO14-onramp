19c7ef5e1368b9a8b8eb9cd31bb645f63e7df672
Lawal Olanrewaju Israel <61704062+lawallanre00490038@users.noreply.github.com>
2022-10-29 15:50:20 +0100

    Update challenge-4.js

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,4 +1,21 @@
 function calculateGrade(marks) {
- 
- }
+    let total = 0;
+    for(let num of marks){
+        total += num;
+    }
+    let avg = total / marks.length;
+    if (avg >= 1 && avg <= 49){
+        return 'F';
+    }else if (avg >= 50 && avg <= 59){
+        return 'E';
+    }else if (avg >= 60 && avg <= 69){
+        return 'D';
+    }else if (avg >= 70 && avg <= 79){
+        return 'C';
+    }else if (avg >= 80 && avg <= 89){
+        return 'B';
+    }else if (avg >= 90 && avg <= 100){
+        return 'A';
+    }
+};
   module.exports =calculateGrade
