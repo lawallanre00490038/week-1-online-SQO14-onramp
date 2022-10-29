45731ea1ae9a35cc9335b6847c4d6ee2971b35c4
Lawal Olanrewaju Israel <61704062+lawallanre00490038@users.noreply.github.com>
2022-10-29 15:20:53 +0100

    Update challenge-2.js

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,6 +1,13 @@
 function fizzBuzz(input){
-
-  
+    if (input % 3 === 0){
+        return "fizz";
+    }else if (input % 5 === 0){
+        return "buzz";
+    }else if (input % 3 === 0 && input % 5 ===0){
+        return "fizbuzz"
+    }else if (input % 3 !==0 && input % 5 !==0){
+        return input
+    }
 }
 
 
